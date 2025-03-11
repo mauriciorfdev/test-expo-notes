aa47da6d381bc7943f09b8d5e85502f34b81085d
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-03-11 19:45:51 -0300

    new folder notes
    notes: index and _layout. Components for notes: FlatList, Modal, TextInput

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -18,7 +18,8 @@ const RootLayout = () => {
       }
     }}
   >
-    <Stack.Screen name="index" options={{title:'Home'}}/>
+    <Stack.Screen name="index" options={{title:'Home'}} />
+    <Stack.Screen name="notes" options={{headerTitle: 'Notes'}} />
   </Stack>
 }
 
