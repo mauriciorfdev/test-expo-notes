457023cf2f4682c4c2a011367f38395068a159c2
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-04-07 10:47:30 -0400

    update NoteItem - Fix TextInput flickering on android
    added new view for wrap textInput and add style of flex:1 because container view has flexDirection: 'row'. Added a useRef for compare original text vs edited to avoid unnecesary updates when the text remains the same

--- components/NoteItem.jsx
+++ components/NoteItem.jsx
@@ -1,38 +1,44 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   TextInput,
-} from "react-native";
+} from 'react-native';
 
 const NoteItem = ({ note, onDelete, onEdit }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedText, setEditedText] = useState(note.text);
   const inputRef = useRef(null);
+  const originalText = useRef(note.text);
 
   const handleSave = () => {
-    if (editedText.trim() === "") return;
-    onEdit(note.$id, editedText);
+    if (editedText.trim() === '') return;
+    if (editedText !== originalText.current) {
+      onEdit(note.$id, editedText);
+      originalText.current = editedText;
+    }
     setIsEditing(false);
   };
 
   return (
     <View style={styles.noteItem}>
-      {isEditing ? (
-        <TextInput
-          ref={inputRef}
-          style={styles.input}
-          value={editedText}
-          onChangeText={setEditedText}
-          autoFocus
-          onSubmitEditing={handleSave}
-          returnKeyType="done"
-        />
-      ) : (
-        <Text style={styles.noteText}> {note.text} </Text>
-      )}
+      <View style={styles.itemText}>
+        {isEditing ? (
+          <TextInput
+            ref={inputRef}
+            style={styles.input}
+            value={editedText}
+            onChangeText={setEditedText}
+            autoFocus
+            onSubmitEditing={handleSave}
+            returnKeyType='done'
+          />
+        ) : (
+          <Text style={styles.noteText}> {note.text} </Text>
+        )}
+      </View>
 
       <View style={styles.actions}>
         {isEditing ? (
@@ -60,9 +66,9 @@ const NoteItem = ({ note, onDelete, onEdit }) => {
 
 const styles = StyleSheet.create({
   noteItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#f5f5f5",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#f5f5f5',
     padding: 15,
     borderRadius: 5,
     marginVertical: 5,
@@ -72,21 +78,24 @@ const styles = StyleSheet.create({
   },
   delete: {
     fontSize: 18,
-    color: "red",
+    color: 'red',
   },
   input: {
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderRadius: 8,
     fontSize: 18,
   },
   update: {
     fontSize: 18,
-    color: "blue",
+    color: 'blue',
     marginRight: 10,
   },
+  itemText: {
+    flex: 1,
+  },
   actions: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
 });
 
